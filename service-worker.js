f1ba3eb36ac561d99dcf33138b9b3d6cbd3198b1
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-07-29 16:41:19 -0400

    Cache center-out cold glue brush integration

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE_VERSION = "0.9.2";
-const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-map-contact-hotfix";
+const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-cold-glue-center-out";
 const CACHE_PREFIX = "servoforge-labeler-staging-";
 const APP_SHELL_URL = new URL("./index.html", self.registration.scope).href;
 
@@ -61,6 +61,8 @@ const CORE_ASSETS = Object.freeze([
   "./app/simulation-collapsible-core.js",
   "./app/multi-map-lock-import-integration-v2.js",
   "./app/map-object-wipe-definition-integration.js",
+  "./app/apl-single-cycle-transition-guard.js",
+  "./app/cold-glue-center-out-brush-integration.js",
   "./app/optimizer-map-contact-integration.js",
   "./app/update-manager.js",
   "./app.js"
