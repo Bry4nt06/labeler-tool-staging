7e18418ce7b7baff5eff252fad05fa9a920a8275
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-08-01 11:41:59 -0400

    Cache generic map-object servo orientation

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE_VERSION = "0.9.2";
-const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-active-label-sensor-reference";
+const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-map-object-orientation-v1";
 const CACHE_PREFIX = "servoforge-labeler-staging-";
 const APP_SHELL_URL = new URL("./index.html", self.registration.scope).href;
 
@@ -70,6 +70,8 @@ const CORE_ASSETS = Object.freeze([
   "./app/apl-body-back-two-label-transition-integration.js",
   "./app/apl-back-wipe-direction-correction-integration.js",
   "./app/apl-label-sensor-reference-integration.js",
+  "./app/map-object-servo-orientation-integration.js",
+  "./app/map-object-orientation-controls-integration.js",
   "./app/cold-glue-label-geometry-fallback-integration.js",
   "./app/cold-glue-center-out-brush-integration.js",
   "./app/cold-glue-gripper-channel-integration.js",
