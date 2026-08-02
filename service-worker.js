1d81c7db27813166c26866bec1bcacd4e2a43358
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-08-02 10:39:48 -0400

    Refresh cache for equivalent coding Rest cleanup

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE_VERSION = "0.9.2";
-const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-rest-grammar-v2";
+const CACHE_NAME = "servoforge-labeler-staging-v0.9.2-rest-grammar-v4";
 const CACHE_PREFIX = "servoforge-labeler-staging-";
 const APP_SHELL_URL = new URL("./index.html", self.registration.scope).href;
 
