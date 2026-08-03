4c7d65e12bd8b862b0c5f89e85f625bf99463282
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-08-03 08:18:21 -0400

    Add Map Builder action controller

---
+++ app/controllers/map-builder-action-controller.js
@@ -0,0 +1,80 @@
+"use strict";
+
+(function installMapBuilderActionController(global) {
+  if (global.LabelerMapBuilderActionController?.installed) return;
+
+  const actions = global.LabelerWorkspaceActionService;
+
+  function call(name, ...args) {
+    return actions?.call(name, ...args);
+  }
+
+  function saveDefinition(eventType = "change") {
+    return call("saveMapDefinitionFromControls", { type: eventType });
+  }
+
+  function updateObjectType() {
+    return call("updateBuilderTypeControls");
+  }
+
+  function undo() {
+    return call("restoreBuilderHistory", "undo");
+  }
+
+  function redo() {
+    return call("restoreBuilderHistory", "redo");
+  }
+
+  function guidedSetup() {
+    return call("runGuidedMapSetup");
+  }
+
+  function optimizeColdGlue() {
+    return call("optimizeActiveColdGlueMap");
+  }
+
+  function createFromCurrent() {
+    return call("createMachineMapFromCurrent");
+  }
+
+  function selectZone(value) {
+    return call("selectMapLibraryZone", value);
+  }
+
+  function selectSite(value) {
+    return call("selectMapLibrarySite", value);
+  }
+
+  function addMachineType() {
+    return call("addMachineTypeFromPrompt");
+  }
+
+  function deleteActiveMap() {
+    return call("deleteActiveMachineMap");
+  }
+
+  function addObject() {
+    return call("addBuilderObjectFromControls");
+  }
+
+  function resetMap() {
+    return call("resetActiveBuilderMap");
+  }
+
+  global.LabelerMapBuilderActionController = Object.freeze({
+    installed: true,
+    saveDefinition,
+    updateObjectType,
+    undo,
+    redo,
+    guidedSetup,
+    optimizeColdGlue,
+    createFromCurrent,
+    selectZone,
+    selectSite,
+    addMachineType,
+    deleteActiveMap,
+    addObject,
+    resetMap
+  });
+})(window);
