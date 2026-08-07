1e145fab7dabc8716773610de92fffae986dc9d8
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-08-07 00:08:18 -0400

    Stamp label centerline policy build

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -2,8 +2,8 @@
 
 (function loadServoForgeBootstrapModules() {
   const version = "0.9.10";
-  const build = "physical-sensor-visibility-20260806-2336";
-  const buildUpdatedAt = "Aug 6, 2026 11:36 PM ET";
+  const build = "label-centerline-policy-20260807-0001";
+  const buildUpdatedAt = "Aug 7, 2026 12:01 AM ET";
   window.SERVOFORGE_RELEASE_VERSION = version;
   window.SERVOFORGE_BUILD_UPDATED_AT = buildUpdatedAt;
   const banner = document.querySelector(".staging-environment-banner");
