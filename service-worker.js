d92ea93cdda96de1bfe9b171ae7319dbd6621960
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-09-03 19:32:46 -0400

    Advance offline troubleshooting cache to v329

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const RELEASE_VERSION = "0.9.10";
-const CACHE_NAME = "servoforge-labeler-staging-v0.9.10-troubleshooting-circuit-trace-v328-20260903-1425";
+const CACHE_NAME = "servoforge-labeler-staging-v0.9.10-troubleshooting-exact-circuit-v329-20260903-1920";
 const CACHE_PREFIX = "servoforge-labeler-staging-";
 const APP_SHELL_URL = new URL("./index.html", self.registration.scope).href;
 
