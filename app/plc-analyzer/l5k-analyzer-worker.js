eebbb73f9dadfdaac3df7add074ccb4fdf42213f
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-09-04 23:27:18 -0400

    PLC Analyzer v8: load communication topology in worker

--- app/plc-analyzer/l5k-analyzer-worker.js
+++ app/plc-analyzer/l5k-analyzer-worker.js
@@ -5,7 +5,8 @@ importScripts(
   "./l5k-analyzer-legacy-v4.js?v=4.1",
   "./l5k-analyzer-dependencies-v5.js?v=5",
   "./l5k-analyzer-discrepancies-v6.js?v=6",
-  "./l5k-analyzer-task-schedule-v7.js?v=7"
+  "./l5k-analyzer-task-schedule-v7.js?v=7",
+  "./l5k-analyzer-communication-v8.js?v=8"
 );
 
 self.addEventListener("message", (event) => {
