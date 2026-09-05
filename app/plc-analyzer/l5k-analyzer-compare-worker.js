962089c48511e6cd7320a3a85acac0862b8c3882
Bry4nt06 <139938543+Bry4nt06@users.noreply.github.com>
2026-09-05 00:31:06 -0400

    PLC Compare v8.1: parse communication topology in worker

--- app/plc-analyzer/l5k-analyzer-compare-worker.js
+++ app/plc-analyzer/l5k-analyzer-compare-worker.js
@@ -6,9 +6,11 @@ importScripts(
   "./l5k-analyzer-dependencies-v5.js?v=5",
   "./l5k-analyzer-discrepancies-v6.js?v=6",
   "./l5k-analyzer-task-schedule-v7.js?v=7",
+  "./l5k-analyzer-communication-v8.js?v=8",
   "./l5k-analyzer-compare.js?v=2",
   "./l5k-analyzer-compare-dependencies-v5-2.js?v=5.2",
-  "./l5k-analyzer-compare-task-schedule-v7-1.js?v=7.1"
+  "./l5k-analyzer-compare-task-schedule-v7-1.js?v=7.1",
+  "./l5k-analyzer-compare-communication-v8-1.js?v=8.1"
 );
 
 self.onmessage = (event) => {
